459b74636d330ea7c5dac8ec34ce476d77e31eee
Pavlo Gorb <p.orbitum@gmail.com>
2015-09-17 00:10:55 +0300

    - Refactored VKUserData.

--- Classes/Models/VKUserData.h
+++ Classes/Models/VKUserData.h
@@ -7,50 +7,17 @@
 //
 
 #import <Foundation/Foundation.h>
-#import "VKBaseModel.h"
+#import <VirgilFrameworkiOS/VFUserData.h>
 
-extern NSString* const kVKUserDataClassUnknown;
-extern NSString* const kVKUserDataClassUserId;
-extern NSString* const kVKUserDataClassUserInfo;
+@class VKIdBundle;
 
-typedef NS_ENUM(NSUInteger, VKUserDataClass) {
-    UDCUnknown,
-    UDCUserId,
-    UDCUserInfo
-};
+@interface VKUserData : VFUserData
 
-extern NSString* const kVKUserDataTypeUnknown;
-extern NSString* const kVKUserDataTypeEmail;
-extern NSString* const kVKUserDataTypeDomain;
-extern NSString* const kVKUserDataTypeApplication;
-extern NSString* const kVKUserDataTypeFirstName;
-extern NSString* const kVKUserDataTypeLastName;
-
-typedef NS_ENUM(NSUInteger, VKUserDataType) {
-    UDTUnknown,
-    UDTFirstName,   // Can be used with Class = user_id.
-    UDTLastName,    // Can be used with Class = user_id.
-    UDTEmail,       // Can be used with Class = user_info.
-    UDTDomain,      // Reserved. Not used at the moment.
-    UDTApplication  // Reserved. Not used at the moment.
-};
-
-@interface VKUserData : VKBaseModel
-
-@property (nonatomic, assign, readonly) VKUserDataClass Class;
-@property (nonatomic, assign, readonly) VKUserDataType Type;
-@property (nonatomic, copy, readonly) NSString *Value;
+@property (nonatomic, copy, readonly) VKIdBundle *Id;
 @property (nonatomic, copy, readonly) NSNumber *Confirmed;
 
-- (instancetype)initWithId:(VKIdBundle *)Id Class:(VKUserDataClass)Class Type:(VKUserDataType)Type Value:(NSString *)Value Confirmed:(NSNumber *)Confirmed NS_DESIGNATED_INITIALIZER;
+- (instancetype)initWithId:(VKIdBundle *)Id Class:(VFUserDataClass)Class Type:(VFUserDataType)Type Value:(NSString *)Value Confirmed:(NSNumber *)Confirmed NS_DESIGNATED_INITIALIZER;
 - (instancetype)initWithUserData:(VKUserData *)userData;
-
-// Convenient methods for converting enums to strings and vice-versa.
-+ (NSString *)stringFromUserDataClass:(VKUserDataClass)Class;
-+ (VKUserDataClass)userDataClassFromString:(NSString *)ClassCandidate;
-
-+ (NSString *)stringFromUserDataType:(VKUserDataType)Type;
-+ (VKUserDataType)userDataTypeFromString:(NSString *)TypeCandidate;
 
 @end
 
--- Classes/Models/VKUserData.m
+++ Classes/Models/VKUserData.m
@@ -7,61 +7,40 @@
 //
 
 #import "VKUserData.h"
+#import "VKIdBundle.h"
 #import <VirgilFrameworkiOS/NSObject+VFUtils.h>
-
-// String equivalents for enums.
-// The following string values will be used in data transfer objects
-
-// String values for UserData class enum.
-NSString* const kVKUserDataClassUnknown = @"unknown";
-NSString* const kVKUserDataClassUserId = @"user_id";
-NSString* const kVKUserDataClassUserInfo = @"user_info";
-
-// String values for UserData type enum.
-NSString* const kVKUserDataTypeUnknown = @"unknown";
-NSString* const kVKUserDataTypeEmail = @"email";
-NSString* const kVKUserDataTypeDomain = @"doimain";
-NSString* const kVKUserDataTypeApplication = @"application";
-NSString* const kVKUserDataTypeFirstName = @"first_name";
-NSString* const kVKUserDataTypeLastName = @"last_name";
 
 @interface VKUserData ()
 
-@property (nonatomic, assign, readwrite) VKUserDataClass Class;
-@property (nonatomic, assign, readwrite) VKUserDataType Type;
-@property (nonatomic, copy, readwrite) NSString *Value;
+@property (nonatomic, copy, readwrite) VKIdBundle *Id;
 @property (nonatomic, copy, readwrite) NSNumber *Confirmed;
 
 @end
 
 @implementation VKUserData
 
-@synthesize Class = _Class;
-@synthesize Type = _Type;
-@synthesize Value = _Value;
+@synthesize Id = _Id;
 @synthesize Confirmed = _Confirmed;
 
 #pragma mark - Lifecycle
 
-- (instancetype)initWithId:(VKIdBundle *)Id Class:(VKUserDataClass)Class Type:(VKUserDataType)Type Value:(NSString *)Value Confirmed:(NSNumber *)Confirmed {
-    self = [super initWithId:Id];
+- (instancetype)initWithId:(VKIdBundle *)Id Class:(VFUserDataClass)Class Type:(VFUserDataType)Type Value:(NSString *)Value Confirmed:(NSNumber *)Confirmed {
+    self = [super initWithClass:Class Type:Type Value:Value];
     if (self == nil) {
         return nil;
     }
-    
-    _Class = Class;
-    _Type = Type;
-    _Value = [Value copy];
+
+    _Id = [Id copy];
     _Confirmed = [Confirmed copy];
     return self;
 }
 
-- (instancetype)initWithId:(VKIdBundle *)Id {
-    return [self initWithId:Id Class:UDCUnknown Type:UDTUnknown Value:nil Confirmed:@NO];
-}
-
 - (instancetype)initWithUserData:(VKUserData *)userData {
     return [self initWithId:userData.Id Class:userData.Class Type:userData.Type Value:userData.Value Confirmed:userData.Confirmed];
+}
+
+- (instancetype)initWithClass:(VFUserDataClass)Class Type:(VFUserDataType)Type Value:(NSString *)Value {
+    return [self initWithId:nil Class:Class Type:Type Value:Value Confirmed:@NO];
 }
 
 - (instancetype)init {
@@ -78,21 +57,19 @@ NSString* const kVKUserDataTypeLastName = @"last_name";
 
 - (instancetype)initWithCoder:(NSCoder *)aDecoder {
     VKIdBundle *Id = [[aDecoder decodeObjectForKey:kVKModelId] as:[VKIdBundle class]];
-    NSNumber *classCandidate = [[aDecoder decodeObjectForKey:kVKModelClass] as:[NSNumber class]];
-    NSNumber *typeCandidate = [[aDecoder decodeObjectForKey:kVKModelType] as:[NSNumber class]];
-    NSString *value = [[aDecoder decodeObjectForKey:kVKModelValue] as:[NSString class]];
+    NSNumber *classCandidate = [[aDecoder decodeObjectForKey:kVFModelClass] as:[NSNumber class]];
+    NSNumber *typeCandidate = [[aDecoder decodeObjectForKey:kVFModelType] as:[NSNumber class]];
+    NSString *value = [[aDecoder decodeObjectForKey:kVFModelValue] as:[NSString class]];
     NSNumber *confirmed = [[aDecoder decodeObjectForKey:kVKModelConfirmed] as:[NSNumber class]];
     
-    return [self initWithId:Id Class:(VKUserDataClass)[classCandidate unsignedIntegerValue] Type:(VKUserDataType)[typeCandidate unsignedIntegerValue] Value:value Confirmed:confirmed];
+    return [self initWithId:Id Class:(VFUserDataClass)[classCandidate unsignedIntegerValue] Type:(VFUserDataType)[typeCandidate unsignedIntegerValue] Value:value Confirmed:confirmed];
 }
 
 - (void)encodeWithCoder:(NSCoder *)aCoder {
     [super encodeWithCoder:aCoder];
     
-    [aCoder encodeObject:[NSNumber numberWithUnsignedInteger:self.Class] forKey:kVKModelClass];
-    [aCoder encodeObject:[NSNumber numberWithUnsignedInteger:self.Type] forKey:kVKModelType];
-    if (self.Value != nil) {
-        [aCoder encodeObject:self.Value forKey:kVKModelValue];
+    if (self.ID != nil) {
+        [aCoder encodeObject:self.Id forKey:kVKModelId];
     }
     if (self.Confirmed != nil) {
         [aCoder encodeObject:self.Confirmed forKey:kVKModelConfirmed];
@@ -101,124 +78,24 @@ NSString* const kVKUserDataTypeLastName = @"last_name";
 
 #pragma mark - VKSerializable
 
-- (NSDictionary *)serialize {
-    NSMutableDictionary *dto = [[NSMutableDictionary alloc] initWithDictionary:[super serialize]];
-    dto[kVKModelClass] = [[self class] stringFromUserDataClass:self.Class];
-    dto[kVKModelType] = [[self class] stringFromUserDataType:self.Type];
-    if (self.Value != nil) {
-        dto[kVKModelValue] = self.Value;
-    }
-    
-    return dto;
-}
-
 + (instancetype)deserializeFrom:(NSDictionary *)candidate {
     NSDictionary *idBundle = [candidate[kVKModelId] as:[NSDictionary class]];
     VKIdBundle *Id = [VKIdBundle deserializeFrom:idBundle];
 
-    NSString *classCandidate = [candidate[kVKModelClass] as:[NSString class]];
-    NSString *typeCandidate = [candidate[kVKModelType] as:[NSString class]];
-    NSString *value = [candidate[kVKModelValue] as:[NSString class]];
+    NSString *classCandidate = [candidate[kVFModelClass] as:[NSString class]];
+    NSString *typeCandidate = [candidate[kVFModelType] as:[NSString class]];
+    NSString *value = [candidate[kVFModelValue] as:[NSString class]];
     NSNumber *confirmed = [candidate[kVKModelConfirmed] as:[NSNumber class]];
     
     return [[self alloc] initWithId:Id Class:[self userDataClassFromString:classCandidate] Type:[self userDataTypeFromString:typeCandidate] Value:value Confirmed:confirmed];
-}
-
-#pragma mark - NSObject protocol implementation: equality checks
-
-- (BOOL)isEqual:(id)object {
-    VKUserData *ud = [object as:[VKUserData class]];
-    if (ud == nil) {
-        return NO;
-    }
-    
-    return ([self.Id isEqual:ud.Id]);
-}
-
-- (NSUInteger)hash {
-    return [self.Id hash];
 }
 
 #pragma mark - Overrides 
 
 - (NSNumber *)isValid {
     BOOL valid = NO;
-    valid = ( (self.Id.userDataId.length > 0) && (self.Value.length > 0) && (self.Class != UDCUnknown) && (self.Type != UDTUnknown) );
+    valid = ( [super isValid] && (self.Id.userDataId.length > 0) );
     return [NSNumber numberWithBool:valid];
-}
-
-#pragma mark - Public utility logic
-
-+ (NSString *)stringFromUserDataClass:(VKUserDataClass)Class {
-    NSString *strClass = nil;
-    switch (Class) {
-        case UDCUserId:
-            strClass = kVKUserDataClassUserId;
-            break;
-        case UDCUserInfo:
-            strClass = kVKUserDataClassUserInfo;
-            break;
-        default:
-            strClass = kVKUserDataClassUnknown;
-            break;
-    }
-    return strClass;
-}
-
-+ (VKUserDataClass)userDataClassFromString:(NSString *)ClassCandidate {
-    VKUserDataClass cls = UDCUnknown;
-    if ([ClassCandidate isEqualToString:kVKUserDataClassUserId]) {
-        cls = UDCUserId;
-    }
-    else if ([ClassCandidate isEqualToString:kVKUserDataClassUserInfo]) {
-        cls = UDCUserInfo;
-    }
-    return cls;
-}
-
-+ (NSString *)stringFromUserDataType:(VKUserDataType)Type {
-    NSString *strType = nil;
-    switch (Type) {
-        case UDTFirstName:
-            strType = kVKUserDataTypeFirstName;
-            break;
-        case UDTLastName:
-            strType = kVKUserDataTypeLastName;
-            break;
-        case UDTEmail:
-            strType = kVKUserDataTypeEmail;
-            break;
-        case UDTDomain:
-            strType = kVKUserDataTypeDomain;
-            break;
-        case UDTApplication:
-            strType = kVKUserDataTypeApplication;
-            break;
-        default:
-            strType = kVKUserDataTypeUnknown;
-            break;
-    }
-    return strType;
-}
-
-+ (VKUserDataType)userDataTypeFromString:(NSString *)TypeCandidate {
-    VKUserDataType tp = UDTUnknown;
-    if ([TypeCandidate isEqualToString:kVKUserDataTypeFirstName]) {
-        tp = UDTFirstName;
-    }
-    else if ([TypeCandidate isEqualToString:kVKUserDataTypeLastName]) {
-        tp = UDTLastName;
-    }
-    else if ([TypeCandidate isEqualToString:kVKUserDataTypeEmail]) {
-        tp = UDTEmail;
-    }
-    else if ([TypeCandidate isEqualToString:kVKUserDataTypeDomain]) {
-        tp = UDTDomain;
-    }
-    else if ([TypeCandidate isEqualToString:kVKUserDataTypeApplication]) {
-        tp = UDTApplication;
-    }
-    return tp;
 }
 
 @end
