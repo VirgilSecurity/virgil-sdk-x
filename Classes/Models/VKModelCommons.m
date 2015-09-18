e201d0a42ad268870f9a696a116328aa8101661b
Pavlo Gorb <p.orbitum@gmail.com>
2015-09-19 00:18:03 +0300

    - Minor improvements

--- Classes/Models/VKModelCommons.m
+++ Classes/Models/VKModelCommons.m
@@ -6,6 +6,8 @@
 //  Copyright (c) 2015 VirgilSecurity. All rights reserved.
 //
 
+#import <Foundation/Foundation.h>
+
 NSString *const kVKModelId = @"id";
 NSString *const kVKModelContainerId = @"account_id";
 NSString *const kVKModelPublicKeyId = @"public_key_id";
