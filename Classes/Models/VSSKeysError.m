997ef561d8d6a83f6c52f5bca1b05b696bf4cf31
Pavlo Gorb <p.orbitum@gmail.com>
2015-10-13 00:46:41 +0300

    - Renamed error constants

--- Classes/Models/VSSKeysError.m
+++ Classes/Models/VSSKeysError.m
@@ -9,189 +9,189 @@
 #import "VSSKeysError.h"
 
 // General errors
-const NSInteger kVKInternalError                             = 10000;
-const NSInteger kVKJSONRequestError                          = 10100;
+const NSInteger kVSSKeysInternalError                             = 10000;
+const NSInteger kVSSKeysJSONRequestError                          = 10100;
 //Request sign errors
-const NSInteger kVKUUIDDuplicateError                        = 10200;
-const NSInteger kVKUUIDError                                 = 10201;
-const NSInteger kVKSignHeaderError                           = 10202;
-const NSInteger kVKPublicKeyHeaderError                      = 10203;
-const NSInteger kVKSignError                                 = 10204;
-const NSInteger kVKPublicKeyUUIDError                        = 10207;
-const NSInteger kVKPublicKeyApplicationViolationError        = 10209;
-const NSInteger kVKPublicKeyBase64EncodingError              = 10210;
-const NSInteger kVKPublicKeyUUIDMatchError                   = 10211;
+const NSInteger kVSSKeysUUIDDuplicateError                        = 10200;
+const NSInteger kVSSKeysUUIDError                                 = 10201;
+const NSInteger kVSSKeysSignHeaderError                           = 10202;
+const NSInteger kVSSKeysPublicKeyHeaderError                      = 10203;
+const NSInteger kVSSKeysSignError                                 = 10204;
+const NSInteger kVSSKeysPublicKeyUUIDError                        = 10207;
+const NSInteger kVSSKeysPublicKeyApplicationViolationError        = 10209;
+const NSInteger kVSSKeysPublicKeyBase64EncodingError              = 10210;
+const NSInteger kVSSKeysPublicKeyUUIDMatchError                   = 10211;
 //Application token errors
-const NSInteger kVKApplicationTokenError                     = 10205;
-const NSInteger kVKStatisticsError                           = 10206;
+const NSInteger kVSSKeysApplicationTokenError                     = 10205;
+const NSInteger kVSSKeysStatisticsError                           = 10206;
 //Endpoints errors
-const NSInteger kVKPublicKeyRequiredError                    = 10208;
-const NSInteger kVKActionTokenError                          = 20010;
-const NSInteger kVKConfirmationCodesNumberError              = 20011;
-const NSInteger kVKConfirmationCodeError                     = 20012;
-const NSInteger kVKPublicKeyIsNotFoundError                  = 20100;
-const NSInteger kVKPublicKeyLengthError                      = 20101;
-const NSInteger kVKPublicKeyError                            = 20102;
-const NSInteger kVKPublicKeyEncodingError                    = 20103;
-const NSInteger kVKPublicKeyUserDataUnconfirmedError         = 20104;
-const NSInteger kVKPublicKeyUserIdError                      = 20105;
-const NSInteger kVKPublicKeyUDIDRegisteredError              = 20107;
-const NSInteger kVKPublicKeyUDIDsRegisteredError             = 20108;
-const NSInteger kVKPublicKeyIsNotFoundForApplicationError    = 20110;
-const NSInteger kVKPublicKeyIsFoundForApplicationError       = 20111;
-const NSInteger kVKPublicKeyIsRegisteredForApplicationError  = 20112;
-const NSInteger kVKUUIDSignVerificationError                 = 20113;
-const NSInteger kVKUserDataIsNotFoundError                   = 20200;
-const NSInteger kVKUserDataTypeError                         = 20202;
-const NSInteger kVKUserDataDomainError                       = 20203;
-const NSInteger kVKUserDataEmailError                        = 20204;
-const NSInteger kVKUserDataPhoneError                        = 20205;
-const NSInteger kVKUserDataConstraintError                   = 20210;
-const NSInteger kVKUserDataConfirmationEntityError           = 20211;
-const NSInteger kVKUserDataConfirmationTokenError            = 20212;
-const NSInteger kVKUserDataConfirmationDuplicateError        = 20213;
-const NSInteger kVKUserDataClassError                        = 20214;
-const NSInteger kVKUserDataDomainValueError                  = 20215;
-const NSInteger kVKUserDataUserIdConfirmationDuplicateError  = 20216;
-const NSInteger kVKUserDataIsNotConfirmedError               = 20217;
-const NSInteger kVKUserDataValueError                        = 20218;
-const NSInteger kVKUserDataUserInfoError                     = 20300;
+const NSInteger kVSSKeysPublicKeyRequiredError                    = 10208;
+const NSInteger kVSSKeysActionTokenError                          = 20010;
+const NSInteger kVSSKeysConfirmationCodesNumberError              = 20011;
+const NSInteger kVSSKeysConfirmationCodeError                     = 20012;
+const NSInteger kVSSKeysPublicKeyIsNotFoundError                  = 20100;
+const NSInteger kVSSKeysPublicKeyLengthError                      = 20101;
+const NSInteger kVSSKeysPublicKeyError                            = 20102;
+const NSInteger kVSSKeysPublicKeyEncodingError                    = 20103;
+const NSInteger kVSSKeysPublicKeyUserDataUnconfirmedError         = 20104;
+const NSInteger kVSSKeysPublicKeyUserIdError                      = 20105;
+const NSInteger kVSSKeysPublicKeyUDIDRegisteredError              = 20107;
+const NSInteger kVSSKeysPublicKeyUDIDsRegisteredError             = 20108;
+const NSInteger kVSSKeysPublicKeyIsNotFoundForApplicationError    = 20110;
+const NSInteger kVSSKeysPublicKeyIsFoundForApplicationError       = 20111;
+const NSInteger kVSSKeysPublicKeyIsRegisteredForApplicationError  = 20112;
+const NSInteger kVSSKeysUUIDSignVerificationError                 = 20113;
+const NSInteger kVSSKeysUserDataIsNotFoundError                   = 20200;
+const NSInteger kVSSKeysUserDataTypeError                         = 20202;
+const NSInteger kVSSKeysUserDataDomainError                       = 20203;
+const NSInteger kVSSKeysUserDataEmailError                        = 20204;
+const NSInteger kVSSKeysUserDataPhoneError                        = 20205;
+const NSInteger kVSSKeysUserDataConstraintError                   = 20210;
+const NSInteger kVSSKeysUserDataConfirmationEntityError           = 20211;
+const NSInteger kVSSKeysUserDataConfirmationTokenError            = 20212;
+const NSInteger kVSSKeysUserDataConfirmationDuplicateError        = 20213;
+const NSInteger kVSSKeysUserDataClassError                        = 20214;
+const NSInteger kVSSKeysUserDataDomainValueError                  = 20215;
+const NSInteger kVSSKeysUserDataUserIdConfirmationDuplicateError  = 20216;
+const NSInteger kVSSKeysUserDataIsNotConfirmedError               = 20217;
+const NSInteger kVSSKeysUserDataValueError                        = 20218;
+const NSInteger kVSSKeysUserDataUserInfoError                     = 20300;
 
 @implementation VSSKeysError
 
 - (NSString *)message {
     NSString *message = nil;
     switch (self.code) {
-        case kVKInternalError:
+        case kVSSKeysInternalError:
             message = @"Internal service error.";
             break;
-        case kVKJSONRequestError:
+        case kVSSKeysJSONRequestError:
             message = @"JSON specified as a request is invalid.";
             break;
-        case kVKUUIDDuplicateError:
+        case kVSSKeysUUIDDuplicateError:
             message = @"The request_sign_uuid parameter was already used for another request.";
             break;
-        case kVKUUIDError:
+        case kVSSKeysUUIDError:
             message = @"The request_sign_uuid parameter is invalid.";
             break;
-        case kVKSignHeaderError:
+        case kVSSKeysSignHeaderError:
             message = @"The request sign header not found.";
             break;
-        case kVKPublicKeyHeaderError:
+        case kVSSKeysPublicKeyHeaderError:
             message = @"The Public Key header not specified or incorrect.";
             break;
-        case kVKSignError:
+        case kVSSKeysSignError:
             message = @"The request sign specified is incorrect.";
             break;
-        case kVKPublicKeyUUIDError:
+        case kVSSKeysPublicKeyUUIDError:
             message = @"The Public Key UUID passed in header was not confirmed yet.";
             break;
-        case kVKPublicKeyApplicationViolationError:
+        case kVSSKeysPublicKeyApplicationViolationError:
             message = @"Public Key specified in authorization header is registered for another application.";
             break;
-        case kVKPublicKeyBase64EncodingError:
+        case kVSSKeysPublicKeyBase64EncodingError:
             message = @"Public Key value in request body for POST /public-key endpoint must be base64 encoded value.";
             break;
-        case kVKPublicKeyUUIDMatchError:
+        case kVSSKeysPublicKeyUUIDMatchError:
             message = @"Public Key UUIDs in URL part and X-VIRGIL-REQUEST-SIGN-PK-ID header must match.";
             break;
-        case kVKApplicationTokenError:
+        case kVSSKeysApplicationTokenError:
             message = @"The Virgil application token not specified or invalid.";
             break;
-        case kVKStatisticsError:
+        case kVSSKeysStatisticsError:
             message = @"The Virgil statistics application error.";
             break;
-        case kVKPublicKeyRequiredError:
+        case kVSSKeysPublicKeyRequiredError:
             message = @"Public Key value required in request body.";
             break;
-        case kVKActionTokenError:
+        case kVSSKeysActionTokenError:
             message = @"Action token is invalid, expired on not found.";
             break;
-        case kVKConfirmationCodesNumberError:
+        case kVSSKeysConfirmationCodesNumberError:
             message = @"Action token's confirmation codes number doesn't match.";
             break;
-        case kVKConfirmationCodeError:
+        case kVSSKeysConfirmationCodeError:
             message = @"One of action token's confirmation codes is invalid.";
             break;
-        case kVKPublicKeyIsNotFoundError:
+        case kVSSKeysPublicKeyIsNotFoundError:
             message = @"Public Key object not found for id specified.";
             break;
-        case kVKPublicKeyLengthError:
+        case kVSSKeysPublicKeyLengthError:
             message = @"Public key length invalid.";
             break;
-        case kVKPublicKeyError:
+        case kVSSKeysPublicKeyError:
             message = @"Public key not specified.";
             break;
-        case kVKPublicKeyEncodingError:
+        case kVSSKeysPublicKeyEncodingError:
             message = @"Public key must be base64-encoded string.";
             break;
-        case kVKPublicKeyUserDataUnconfirmedError:
+        case kVSSKeysPublicKeyUserDataUnconfirmedError:
             message = @"Public key must contain confirmed UserData entities.";
             break;
-        case kVKPublicKeyUserIdError:
+        case kVSSKeysPublicKeyUserIdError:
             message = @"Public key must contain at least one 'user ID' entry.";
             break;
-        case kVKPublicKeyUDIDRegisteredError:
+        case kVSSKeysPublicKeyUDIDRegisteredError:
             message = @"There is UDID registered for current application already.";
             break;
-        case kVKPublicKeyUDIDsRegisteredError:
+        case kVSSKeysPublicKeyUDIDsRegisteredError:
             message = @"UDIDs specified are registered for several accounts.";
             break;
-        case kVKPublicKeyIsNotFoundForApplicationError:
+        case kVSSKeysPublicKeyIsNotFoundForApplicationError:
             message = @"Public key is not found for any application.";
             break;
-        case kVKPublicKeyIsFoundForApplicationError:
+        case kVSSKeysPublicKeyIsFoundForApplicationError:
             message = @"Public key is found for another application.";
             break;
-        case kVKPublicKeyIsRegisteredForApplicationError:
+        case kVSSKeysPublicKeyIsRegisteredForApplicationError:
             message = @"Public key is registered for another application.";
             break;
-        case kVKUUIDSignVerificationError:
+        case kVSSKeysUUIDSignVerificationError:
             message = @"Sign verification failed for request UUID parameter in PUT /public-key.";
             break;
-        case kVKUserDataIsNotFoundError:
+        case kVSSKeysUserDataIsNotFoundError:
             message = @"User Data object not found for id specified.";
             break;
-        case kVKUserDataTypeError:
+        case kVSSKeysUserDataTypeError:
             message = @"User Data type specified as user identity is invalid.";
             break;
-        case kVKUserDataDomainError:
+        case kVSSKeysUserDataDomainError:
             message = @"Domain value specified for the domain identity is invalid.";
             break;
-        case kVKUserDataEmailError:
+        case kVSSKeysUserDataEmailError:
             message = @"Email value specified for the email identity is invalid.";
             break;
-        case kVKUserDataPhoneError:
+        case kVSSKeysUserDataPhoneError:
             message = @"Phone value specified for the phone identity is invalid.";
             break;
-        case kVKUserDataConstraintError:
+        case kVSSKeysUserDataConstraintError:
             message = @"User Data integrity constraint violation.";
             break;
-        case kVKUserDataConfirmationEntityError:
+        case kVSSKeysUserDataConfirmationEntityError:
             message = @"User Data confirmation entity not found.";
             break;
-        case kVKUserDataConfirmationTokenError:
+        case kVSSKeysUserDataConfirmationTokenError:
             message = @"User Data confirmation token invalid.";
             break;
-        case kVKUserDataConfirmationDuplicateError:
+        case kVSSKeysUserDataConfirmationDuplicateError:
             message = @"User Data was already confirmed and does not need further confirmation.";
             break;
-        case kVKUserDataClassError:
+        case kVSSKeysUserDataClassError:
             message = @"User Data class specified is invalid.";
             break;
-        case kVKUserDataDomainValueError:
+        case kVSSKeysUserDataDomainValueError:
             message = @"Domain value specified for the domain identity is invalid.";
             break;
-        case kVKUserDataUserIdConfirmationDuplicateError:
+        case kVSSKeysUserDataUserIdConfirmationDuplicateError:
             message = @"This user id had been confirmed earlier.";
             break;
-        case kVKUserDataIsNotConfirmedError:
+        case kVSSKeysUserDataIsNotConfirmedError:
             message = @"The user data is not confirmed yet.";
             break;
-        case kVKUserDataValueError:
+        case kVSSKeysUserDataValueError:
             message = @"The user data value is required.";
             break;
-        case kVKUserDataUserInfoError:
+        case kVSSKeysUserDataUserInfoError:
             message = @"User info data validation failed.";
             break;
         default:
