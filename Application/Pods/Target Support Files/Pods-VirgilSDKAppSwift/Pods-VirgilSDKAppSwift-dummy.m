6fa2a1a96b434451ebb792f9f6a48667551d1f4a
Yaroslav Tytarenko <yaros8t@gmail.com>
2016-09-18 19:58:58 +0300

    remove pods from repository

--- Application/Pods/Target Support Files/Pods-VirgilSDKAppSwift/Pods-VirgilSDKAppSwift-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_VirgilSDKAppSwift : NSObject
-@end
-@implementation PodsDummy_Pods_VirgilSDKAppSwift
-@end
--- Application/Pods/Target Support Files/Pods-VirgilSDKTests/Pods-VirgilSDKTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_VirgilSDKTests : NSObject
-@end
-@implementation PodsDummy_Pods_VirgilSDKTests
-@end
