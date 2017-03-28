1f819ed9c468a6bf7625c525430c4ed68e473f1d
Oleksandr Deundiak <Deundiak@gmail.com>
2017-03-28 18:02:03 +0300

    Documented VSSVirgilApi

--- Source/HighLevel/VSSVirgilApi.h
+++ Source/HighLevel/VSSVirgilApi.h
@@ -14,10 +14,25 @@
  */
 extern NSString * __nonnull const kVSSVirgilApiErrorDomain;
 
+/**
+ Default implementation of VSSVirgilApi protocol.
+ */
 @interface VSSVirgilApi : NSObject <VSSVirgilApi>
 
+/**
+ Initializes instance with context.
+
+ @param context VSSVirgilApiContext with all needed dependencies. See VSSVirgilApiContext.
+ @return initialized VSSVirgilApiContext instance
+ */
 - (instancetype __nonnull)initWithContext:(VSSVirgilApiContext * __nonnull)context NS_DESIGNATED_INITIALIZER;
 
+/**
+ Initializes instance with context.
+
+ @param token access token to Virgil Service which can be obtained here https://developer.virgilsecurity.com/dashboard/
+ @return initialized VSSVirgilApiContext instance
+ */
 - (instancetype __nonnull)initWithToken:(NSString * __nullable)token;
 
 @end
--- Source/HighLevel/VSSVirgilApiProtocol.h
+++ Source/HighLevel/VSSVirgilApiProtocol.h
@@ -10,13 +10,49 @@
 #import "VSSKeysManagerProtocol.h"
 #import "VSSIdentitiesManagerProtocol.h"
 
+/**
+ Entry point for all interactions with VirgilSDK.
+ See default implementation in @interface VSSVirgilApi.
+ */
 @protocol VSSVirgilApi <NSObject>
 
+/**
+ Entry point for all interactions with Identities.
+ See default implementation: VSSIdentitiesManager.
+ */
 @property (nonatomic, readonly) id<VSSIdentitiesManager> __nonnull Identities;
+
+/**
+ Entry point for all interactions with Virgil Cards.
+ See default implementation: VSSCardsManager.
+ */
 @property (nonatomic, readonly) id<VSSCardsManager> __nonnull Cards;
+
+/**
+ Entry point for all interactions with Virgil Keys.
+ See default implementation: VSSKeysManager.
+ */
 @property (nonatomic, readonly) id<VSSKeysManager> __nonnull Keys;
 
+/**
+ Encrypts data for given recipients.
+
+ @param data NSData to be encrypted
+ @param cards NSArray of Virgil Cards whose owners will be able to decrypt data
+ @param errorPtr NSError pointer to return error if needed
+ @return NSData with encrypted data
+ */
 - (NSData * __nullable)encryptData:(NSData * __nonnull)data for:(NSArray<VSSVirgilCard *> * __nonnull)cards error:(NSError * __nullable * __nullable)errorPtr NS_SWIFT_NAME(encrypt(_:for:));
+
+/**
+ Encrypts string for given recipients.
+ NOTE: while encrypting string is converted to binary representation using UTF8 encoding.
+
+ @param string NSString to be encrypted
+ @param cards NSArray of Virgil Cards whose owners will be able to decrypt data
+ @param errorPtr NSError pointer to return error if needed
+ @return NSData with encrypted string
+ */
 - (NSData * __nullable)encryptString:(NSString * __nonnull)string for:(NSArray<VSSVirgilCard *> * __nonnull)cards error:(NSError * __nullable * __nullable)errorPtr NS_SWIFT_NAME(encrypt(_:for:));
 
 @end
