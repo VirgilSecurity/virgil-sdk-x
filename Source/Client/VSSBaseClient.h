df65801f7f868a44cfab39c9fe0b69a813bdf7cf
Oleksandr Deundiak <Deundiak@gmail.com>
2017-06-12 17:30:36 +0300

    Made networking classes public

--- Source/Client/VSSBaseClient.h
+++ Source/Client/VSSBaseClient.h
@@ -6,9 +6,17 @@
 //  Copyright © 2017 VirgilSecurity. All rights reserved.
 //
 
+#import "VSSBaseClient.h"
+#import "VSSHTTPRequest.h"
+
 /**
  Base class for all Clients.
  */
 @interface VSSBaseClient : NSObject
+
+@property (nonatomic) NSOperationQueue * __nonnull queue;
+@property (nonatomic) NSURLSession * __nonnull urlSession;
+
+- (void)send:(VSSHTTPRequest * __nonnull)request;
 
 @end
