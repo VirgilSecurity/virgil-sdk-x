645ce02a59a61a5a4270e5b210e190d598f49f66
Oleksandr Deundiak <Deundiak@gmail.com>
2017-08-22 16:33:04 +0300

    Revert "Added Store&Delete multuple keys initial version"
    This reverts commit c9e5b51d5e8fe6dc290a675096a366a8c6d9bd3e.

--- Source/KeyStorage/iOS/VSSKeyStorage.h
+++ Source/KeyStorage/iOS/VSSKeyStorage.h
@@ -46,22 +46,4 @@ extern NSString * __nonnull const kVSSKeyStorageErrorDomain;
  */
 - (NSArray<NSData *> * __nullable)getAllKeysTagsWithError:(NSError * __nullable * __nullable)errorPtr;
 
-/**
- Stores multiple key entries.
- 
- @param keyEntries NSArray with VSSKeyEntry instances to store
- @param errorPtr NSError pointer to return error if needed
- @return YES if storing succeeded, NO otherwise
- */
-- (BOOL)storeKeyEntries:(NSArray<VSSKeyEntry *> * __nonnull)keyEntries error:(NSError * __nullable * __nullable)errorPtr;
-
-/**
- Removes multiple key entries with given names.
-
- @param names NSArray with names
- @param errorPtr NSError pointer to return error if needed
- @return YES if succeeded, NO otherwise
- */
-- (BOOL)deleteKeyEntriesWithNames:(NSArray<NSString *> * __nonnull)names error:(NSError * __nullable * __nullable)errorPtr;
-
 @end
--- Source/KeyStorage/iOS/VSSKeyStorage.m
+++ Source/KeyStorage/iOS/VSSKeyStorage.m
@@ -17,7 +17,6 @@ static NSString *privateKeyIdentifierFormat = @".%@.privatekey.%@\0";
 @interface VSSKeyStorage ()
 
 - (NSMutableDictionary * __nonnull)baseKeychainQueryForName:(NSString * __nonnull)name;
-- (NSMutableDictionary * __nonnull)baseKeychainQueryForNames:(NSArray<NSString *> * __nonnull)names;
 - (NSMutableDictionary * __nonnull)baseExtendedKeychainQueryForName:(NSString * __nonnull)name;
 
 @end
@@ -49,48 +48,6 @@ static NSString *privateKeyIdentifierFormat = @".%@.privatekey.%@\0";
         }];
     
     [query addEntriesFromDictionary:keySpecificData];
-    
-    OSStatus status = SecItemAdd((__bridge CFDictionaryRef)query, nil);
-    
-    if (status != errSecSuccess) {
-        if (errorPtr != nil) {
-            *errorPtr = [[NSError alloc] initWithDomain:kVSSKeyStorageErrorDomain code:status userInfo:@{ NSLocalizedDescriptionKey: @"Error while storing key in the keychain. See \"Security Error Codes\" (SecBase.h)." }];
-        }
-        
-        return NO;
-    }
-    
-    return YES;
-}
-
-- (BOOL)storeKeyEntries:(NSArray<VSSKeyEntry *> * __nonnull)keyEntries error:(NSError * __nullable * __nullable)errorPtr {
-    // FIXME: Doesn't work
-    
-    NSMutableArray *queries = [[NSMutableArray alloc] initWithCapacity:keyEntries.count];
-    
-    for (VSSKeyEntry *keyEntry in keyEntries) {
-        NSMutableDictionary *query = [self baseExtendedKeychainQueryForName:keyEntry.name];
-        
-        NSData *keyEntryData = [NSKeyedArchiver archivedDataWithRootObject:keyEntry];
-        NSMutableDictionary *keySpecificData = [NSMutableDictionary dictionaryWithDictionary:
-            @{
-              (__bridge id)kSecValueData: keyEntryData,
-              }];
-        
-        [query addEntriesFromDictionary:keySpecificData];
-        
-        [queries addObject:query];
-    }
-    
-    NSDictionary *query;
-    if (keyEntries.count > 1) {
-        query = @{
-                  (__bridge id)kSecUseItemList: queries
-                  };
-    }
-    else {
-        query = queries[0];
-    }
     
     OSStatus status = SecItemAdd((__bridge CFDictionaryRef)query, nil);
     
@@ -187,23 +144,7 @@ static NSString *privateKeyIdentifierFormat = @".%@.privatekey.%@\0";
     
     if (status != errSecSuccess) {
         if (errorPtr != nil) {
-            *errorPtr = [[NSError alloc] initWithDomain:kVSSKeyStorageErrorDomain code:status userInfo:@{ NSLocalizedDescriptionKey: @"Error while deleting key from the keychain. See \"Security Error Codes\" (SecBase.h)." }];
-        }
-        return NO;
-    }
-    
-    return YES;
-}
-
-- (BOOL)deleteKeyEntriesWithNames:(NSArray<NSString *> *)names error:(NSError **)errorPtr {
-    // FIXME: Doesn't work
-    NSMutableDictionary *query = [self baseKeychainQueryForNames:names];
-    
-    OSStatus status = SecItemDelete((__bridge CFDictionaryRef)query);
-    
-    if (status != errSecSuccess) {
-        if (errorPtr != nil) {
-            *errorPtr = [[NSError alloc] initWithDomain:kVSSKeyStorageErrorDomain code:status userInfo:@{ NSLocalizedDescriptionKey: @"Error while deleting keys from the keychain. See \"Security Error Codes\" (SecBase.h)." }];
+            *errorPtr = [[NSError alloc] initWithDomain:kVSSKeyStorageErrorDomain code:status userInfo:@{ NSLocalizedDescriptionKey: @"Error while obtaining key from the keychain. See \"Security Error Codes\" (SecBase.h)." }];
         }
         return NO;
     }
@@ -226,7 +167,7 @@ static NSString *privateKeyIdentifierFormat = @".%@.privatekey.%@\0";
     
     if (status != errSecSuccess) {
         if (errorPtr != nil) {
-            *errorPtr = [[NSError alloc] initWithDomain:kVSSKeyStorageErrorDomain code:status userInfo:@{ NSLocalizedDescriptionKey: @"Error while getting keys from the keychain. See \"Security Error Codes\" (SecBase.h)." }];
+            *errorPtr = [[NSError alloc] initWithDomain:kVSSKeyStorageErrorDomain code:status userInfo:@{ NSLocalizedDescriptionKey: @"Error while searching keys in the keychain. See \"Security Error Codes\" (SecBase.h)." }];
         }
         return nil;
     }
@@ -259,7 +200,7 @@ static NSString *privateKeyIdentifierFormat = @".%@.privatekey.%@\0";
     
     if (status != errSecSuccess) {
         if (errorPtr != nil) {
-            *errorPtr = [[NSError alloc] initWithDomain:kVSSKeyStorageErrorDomain code:status userInfo:@{ NSLocalizedDescriptionKey: @"Error while getting keys' tags from the keychain. See \"Security Error Codes\" (SecBase.h)." }];
+            *errorPtr = [[NSError alloc] initWithDomain:kVSSKeyStorageErrorDomain code:status userInfo:@{ NSLocalizedDescriptionKey: @"Error while searching keys in the keychain. See \"Security Error Codes\" (SecBase.h)." }];
         }
         return nil;
     }
@@ -275,45 +216,6 @@ static NSString *privateKeyIdentifierFormat = @".%@.privatekey.%@\0";
     }
     
     return keysTags;
-}
-
-- (NSMutableDictionary *)baseExtendedKeychainQueryForNames:(NSArray<NSString *> * __nonnull)names {
-    NSMutableArray *queries = [[NSMutableArray alloc] initWithCapacity:names.count];
-    
-    for (NSString *name in names) {
-        NSMutableDictionary *query = [self baseKeychainQueryForName:name];
-        
-        NSMutableDictionary *additional = [NSMutableDictionary dictionaryWithDictionary:
-               @{
-                 (__bridge id)kSecAttrAccessible: (__bridge id)kSecAttrAccessibleAfterFirstUnlock,
-                 (__bridge id)kSecAttrLabel: name,
-                 (__bridge id)kSecAttrIsPermanent: (__bridge id)kCFBooleanTrue,
-                 (__bridge id)kSecAttrCanEncrypt: (__bridge id)kCFBooleanTrue,
-                 (__bridge id)kSecAttrCanDecrypt: (__bridge id)kCFBooleanFalse,
-                 (__bridge id)kSecAttrCanDerive: (__bridge id)kCFBooleanFalse,
-                 (__bridge id)kSecAttrCanSign: (__bridge id)kCFBooleanTrue,
-                 (__bridge id)kSecAttrCanVerify: (__bridge id)kCFBooleanFalse,
-                 (__bridge id)kSecAttrCanWrap: (__bridge id)kCFBooleanFalse,
-                 (__bridge id)kSecAttrCanUnwrap: (__bridge id)kCFBooleanFalse,
-                 (__bridge id)kSecAttrSynchronizable: (__bridge id)kCFBooleanFalse,
-                 }];
-        
-        // Access groups are not supported in simulator
-#if TARGET_OS_IPHONE && !TARGET_IPHONE_SIMULATOR
-        if (self.configuration.accessGroup != nil) {
-            additional[(__bridge id)kSecAttrAccessGroup] = self.configuration.accessGroup;
-        }
-#endif
-        
-        [query addEntriesFromDictionary:additional];
-        
-        [queries addObject:query];
-    }
-    
-    return [NSMutableDictionary dictionaryWithDictionary:
-        @{
-          (__bridge id)kSecUseItemList: queries
-          }];
 }
 
 - (NSMutableDictionary *)baseExtendedKeychainQueryForName:(NSString *)name {
@@ -344,30 +246,6 @@ static NSString *privateKeyIdentifierFormat = @".%@.privatekey.%@\0";
     [query addEntriesFromDictionary:additional];
     
     return query;
-}
-
-- (NSMutableDictionary *)baseKeychainQueryForNames:(NSArray<NSString *> *)names {
-    NSMutableArray *queries = [[NSMutableArray alloc] initWithCapacity:names.count];
-    
-    for (NSString *name in names) {
-        NSString *tag = [[NSString alloc] initWithFormat:privateKeyIdentifierFormat, self.configuration.applicationName, name];
-        NSData *tagData = [tag dataUsingEncoding:NSUTF8StringEncoding];
-        
-        NSMutableDictionary *query = [NSMutableDictionary dictionaryWithDictionary:
-          @{
-            (__bridge id)kSecClass: (__bridge id)kSecClassKey,
-            (__bridge id)kSecAttrKeyClass: (__bridge id)kSecAttrKeyClassPrivate,
-            (__bridge id)kSecAttrApplicationLabel: [name dataUsingEncoding:NSUTF8StringEncoding],
-            (__bridge id)kSecAttrApplicationTag: tagData
-            }];
-        
-        [queries addObject:query];
-    }
-    
-    return [NSMutableDictionary dictionaryWithDictionary:
-    @{
-      (__bridge id)kSecUseItemList: queries
-      }];
 }
 
 - (NSMutableDictionary *)baseKeychainQueryForName:(NSString *)name {
