cab1ce77e99781630a21ff9735bc16cc28fe26de
Oleksandr Deundiak <Deundiak@gmail.com>
2017-08-22 15:05:14 +0300

    Documented new ios KeyStorage methods

--- Source/KeyStorage/iOS/VSSKeyStorage.h
+++ Source/KeyStorage/iOS/VSSKeyStorage.h
@@ -23,15 +23,27 @@ extern NSString * __nonnull const kVSSKeyStorageErrorDomain;
 @property (nonatomic, copy, readonly) VSSKeyStorageConfiguration * __nonnull configuration;
 
 /**
- Initialized
+ Initializer.
 
  @param configuration Configuration
  @return initialized VSSKeyStorage instance
  */
 - (instancetype __nonnull)initWithConfiguration:(VSSKeyStorageConfiguration * __nonnull)configuration NS_DESIGNATED_INITIALIZER;
 
+/**
+ Returns all keys in the storage.
+
+ @param errorPtr NSError pointer to return error if needed
+ @return NSArray with all keys
+ */
 - (NSArray<VSSKeyEntry *> * __nullable)getAllKeysWithError:(NSError * __nullable * __nullable)errorPtr;
 
-- (NSArray<NSData *> * __nullable)getAllKeysTagsWithError:(NSError * __nullable * __nullable)errorPtr;;
+/**
+ Returns all keys tags in the storage.
+
+ @param errorPtr NSError pointer to return error if needed
+ @return NSArray with all tags
+ */
+- (NSArray<NSData *> * __nullable)getAllKeysTagsWithError:(NSError * __nullable * __nullable)errorPtr;
 
 @end
