6d021990aeb14846e34d061a9ffcf70e1433ffdf
Eugen Pivovarov <epivovarov@virgilsecurity.com>
2018-01-09 13:37:47 +0200

    Added access token protocols

---
+++ Source/JsonWebToken/Protocols/AccessToken.swift
@@ -0,0 +1,27 @@
+//
+//  AccessToken.swift
+//  VirgilSDK
+//
+//  Created by Eugen Pivovarov on 1/9/18.
+//  Copyright © 2018 VirgilSecurity. All rights reserved.
+//
+
+import Foundation
+
+/// This protocol represents Access Token
+@objc(VSSAccessToken) public protocol AccessToken {
+    /// Provides string representation of token
+    ///
+    /// - Returns: string with token
+    @objc func stringRepresentation() -> String
+    
+    /// Extracts identity
+    ///
+    /// - Returns: identity
+    @objc func identity() -> String
+    
+    /// Provides info about expiration
+    ///
+    /// - Returns: true if token is expired
+    @objc func isExpired() -> Bool
+}
---
+++ Source/JsonWebToken/Protocols/AccessTokenProvider.swift
@@ -0,0 +1,18 @@
+//
+//  AccessTokenProvider.swift
+//  VirgilSDK
+//
+//  Created by Eugen Pivovarov on 1/9/18.
+//  Copyright © 2018 VirgilSecurity. All rights reserved.
+//
+
+import Foundation
+
+/// This protocol is responsible for providing AccessToken
+@objc(VSSAccessTokenProvider) public protocol AccessTokenProvider {
+    /// Provides access token
+    ///
+    /// - Parameter forceReload: true will invalidate cached token
+    /// - Returns: access token
+    @objc func getToken(forceReload: Bool) -> AccessToken
+}
