f4044c2a43853a1be1a94707fdd38bf933aa58ed
Oleksandr Deundiak <Deundiak@gmail.com>
2018-02-15 01:35:08 +0200

    GetCardResponse documentation fixes

--- Source/Client/Models/GetCardResponse.swift
+++ Source/Client/Models/GetCardResponse.swift
@@ -12,14 +12,14 @@ import Foundation
 @objc(VSSGetCardResponse) public final class GetCardResponse: NSObject {
     /// RawSignedModel of Virgil Card
     @objc public let rawCard: RawSignedModel
-    /// True if Virgil Card is outdated
+    /// True if Virgil Card is outdated, false - otherwise
     @objc public let isOutdated: Bool
 
-    /// Creates a new `GetCardResponse` with the provided parameters
+    /// Initializer
     ///
     /// - Parameters:
     ///   - rawCard: `RawSignedModel` of Virgil Card
-    ///   - isOutdated: whether or not card is outdated
+    ///   - isOutdated: Whether card is outdated or not
     public init(rawCard: RawSignedModel, isOutdated: Bool) {
         self.rawCard = rawCard
         self.isOutdated = isOutdated
