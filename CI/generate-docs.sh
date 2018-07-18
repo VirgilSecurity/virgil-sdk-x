e00ac780d49349bfbe5088116bf138a6f9bc0e77
Oleksandr Deundiak <Deundiak@gmail.com>
2018-07-18 12:34:12 +0300

    Fixing documentation deploy

--- CI/generate-docs.sh
+++ CI/generate-docs.sh
@@ -1,28 +1,10 @@
-# Load appledoc
-mkdir ${TRAVIS_BUILD_DIR}/appledoc;
-curl -sL https://github.com/tomaz/appledoc/releases/download/2.2.1/appledoc-2.2.1.zip | tar xz -C ${TRAVIS_BUILD_DIR}/appledoc;
+gem install jazzy
 
-# Start constants
-company="Virgil Security";
-companyID="com.virgilsecurity";
-companyURL="https://virgilsecurity.com/";
-target="iphoneos";
-outputPath="${VIRGIL_SDK_HTML_PATH_DST}";
-projectName="VirgilSDK"
-# End constants
-${TRAVIS_BUILD_DIR}/appledoc/appledoc/appledoc \
---project-name "${projectName}" \
---project-company "${company}" \
---company-id "${companyID}" \
---output "${outputPath}" \
---logformat xcode \
---keep-intermediate-files \
---no-repeat-first-par \
---no-warn-invalid-crossref \
---exit-threshold 2 \
---no-create-docset \
---clean-output \
-"${TRAVIS_BUILD_DIR}/Source";
-
-mv ${VIRGIL_SDK_HTML_PATH_DST}/html/* ${VIRGIL_SDK_HTML_PATH_DST};
-rm -r ${VIRGIL_SDK_HTML_PATH_DST}/html;
+jazzy \
+--author "Virgil Security" \
+--author_url "https://virgilsecurity.com/" \
+--xcodebuild-arguments -scheme,"VirgilSDK macOS" \
+--module "VirgilSDK" \
+--output "docs" \
+--hide-documentation-coverage \
+--theme apple
