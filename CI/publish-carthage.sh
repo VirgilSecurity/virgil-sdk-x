5d7c4d85be71c34d3372a9ca96ed3293aebac003
Yevhen Pyvovarov <evgenypyvovarov@gmail.com>
2021-08-07 16:13:37 +0300

    Fixed publish-carthage.sh

--- CI/publish-carthage.sh
+++ CI/publish-carthage.sh
@@ -4,5 +4,5 @@ carthage build --use-xcframeworks --no-skip-current;
 
 # TODO: Should be replaced by carthage archive, when it supports xcframeworks
 FRAMEWORKS_PATH=Carthage/Build
-find ${FRAMEWORKS_PATH} ! -name 'VirgilSDK.xcframework' -delete
+find ${FRAMEWORKS_PATH} -mindepth 1 -maxdepth 1 ! -name 'VirgilSDK.xcframework' -exec rm -rvf {} \;
 zip -r VirgilSDK.xcframework.zip ${FRAMEWORKS_PATH}
