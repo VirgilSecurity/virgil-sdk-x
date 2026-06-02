55889dc27c3b22669d34250ff503cee79ea816e6
Sergii Siroshtan <sergey.seroshtan@gmail.com>
2026-06-02 15:16:13 -0400

    Bump virgil-crypto-x to 7.2.3 (virgil-crypto-c 0.19.1 stable)

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     ],
 
     dependencies: [
-        .package(url: "https://github.com/VirgilSecurity/virgil-crypto-x.git", exact: "7.2.2")
+        .package(url: "https://github.com/VirgilSecurity/virgil-crypto-x.git", exact: "7.2.3")
     ],
 
     targets: [
